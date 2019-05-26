e3adf562265ba57dcde47da15d928cb346537b9d
Joe Ganobsik <jganobsik@gmail.com>
2019-05-26 17:03:57 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -40,14 +40,11 @@ function getRepositories() {
 
 
 function showRepositories() {
-  console.log(this.responseText);
-  let repoList = '<ul>';
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-  }
-  repoList += '</ul>';
-  document.getElementById('repositories').innerHTML = repoList;
-}
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;}
  
 function getRepositories() {
   const req = new XMLHttpRequest();
